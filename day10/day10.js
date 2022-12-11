73346e8315bc2a99c6630cf5992fac0806d83089
Crelde <crelde@hotmail.com>
2022-12-11 11:16:26 +0100

    fix output line for day10

--- day10/day10.js
+++ day10/day10.js
@@ -1,150 +1,4 @@
 const fs = require("fs");
-const input = `addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop`;
 const signalStrengthSum = (input, endAtCycle, func) => {
   let cycleMap = [1];
   let operations = input.split("\n").map((e) => e.split(" "));
@@ -159,17 +13,19 @@ const signalStrengthSum = (input, endAtCycle, func) => {
       cycleMap[currentCycle] = undefined;
     }
   });
-  console.log(func(cycleMap, endAtCycle));
+  return func(cycleMap, endAtCycle);
 };
 const drawLines = (cycleArr, endAtCycle) => {
   let currentRound = 0;
+  let lines = "";
   while (currentRound * endAtCycle + endAtCycle <= cycleArr.length) {
-    drawLine(cycleArr, endAtCycle, currentRound);
+    lines += drawLine(cycleArr, endAtCycle, currentRound);
     currentRound++;
   }
+  return lines;
 };
 const drawLine = (cycleArr, endAtCycle, round) => {
-  let line = "";
+  let line = "\n";
   for (let i = 1; i < endAtCycle; i++) {
     line += drawPixel(
       cycleArr
@@ -178,16 +34,10 @@ const drawLine = (cycleArr, endAtCycle, round) => {
       i
     );
   }
-  console.log(line);
+  return line;
 };
 const drawPixel = (x, i) => {
   //if sprite in range
-  //   console.log(
-  //     "X: " + x,
-  //     "Cycle: " + i,
-  //     "Pos: " + (i - 1),
-  //     "Diff: " + Math.abs(i - x)
-  //   );
   if (Math.abs(i - 1 - x) < 2) {
     return "#";
   } else {
